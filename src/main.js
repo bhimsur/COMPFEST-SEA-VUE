0a6aa81807e2adfecc8c7dc283fec1525e4a7eae
bhimantoro <bhimantoros@gmail.com>
2021-07-08 13:15:58 +0700

    vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,48 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+import AppointmentIndex from './components/appointment/Index'
+import AppointmentCreate from './components/appointment/Create'
+import AppointmentEdit from './components/appointment/Edit'
+
+import AuthLogin from './components/auth/Login'
+import AuthRegister from './components/auth/Register'
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: AuthLogin
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: AuthRegister
+    },
+    {
+      path: '/appointment/create',
+      name: 'create',
+      component: AppointmentCreate
+    },
+    {
+      path: '/appointment/edit/:id',
+      name: 'edit',
+      component: AppointmentEdit
+    },
+    {
+      path: '/appointment',
+      name: 'appointment',
+      component: AppointMentIndex
+    },
+  ],
+  mode: 'history'
+})
 
 new Vue({
   render: h => h(App),
