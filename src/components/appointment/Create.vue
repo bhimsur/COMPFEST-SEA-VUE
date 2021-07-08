887ba8aaf94a50809e4b39c9c20e9b80d2a2f02f
bhimantoro <bhimantoros@gmail.com>
2021-07-08 13:10:06 +0700

    base component

---
+++ src/components/appointment/Create.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="add-appointment">Component Add Appointment</div>
+</template>
+<script>
+export default {};
+</script>
---
+++ src/components/appointment/Edit.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="edit-appointment">Component Edit Appointment</div>
+</template>
+<script>
+export default {};
+</script>
