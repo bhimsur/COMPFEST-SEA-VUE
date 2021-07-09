6abdd93a84ff494fbde97cfc421c80e117aad43a
bhimantoro <bhimantoros@gmail.com>
2021-07-09 19:30:15 +0700

    push

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
 app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-const port = process.env.PORT || 80
+app.use('/', serveStatic(path.join(__dirname, 'dist')))
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+const port = process.env.PORT || 8080
 app.listen(port)
+console.log(`App is listeing on port: ${port}`)
